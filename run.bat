14e0688eda7e282f8b1b5564dbf84be448d7720e
eng-girl <sanuretaroro2001@gmail.com>
2024-10-25 21:54:37 +0300

    last update

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo OFF
 start cmd /k "php artisan serve"
-start cmd /K "C:\Users\Moham\Desktop\gp24\webapp-technician\MailHog_windows_amd64.exe"
+start cmd /K "C:\Users\rahma\Desktop\graduation_ham\MailHog_windows_amd64.exe"
 start cmd /k "php artisan schedule:work"
--- runs.bat
+++ runs.bat
@@ -1,4 +1,4 @@
 @echo OFF
 start cmd /k "php artisan serve"
 start cmd /K "C:\Users\Moham\Desktop\gp24\webapp-technician\MailHog_windows_amd64.exe"
-start cmd /k "php artisan schedule:run > NUL 2>&1"
+start cmd /k "php artisan schedule:work > NUL 2>&1"
